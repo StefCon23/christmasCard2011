2adf19a76d9d4ea4d9034522469034d526beca6e
ConorStefanini <conorstefanini@gmail.com>
2015-06-08 05:40:24 +0100

    debugging version will be changed soon

--- README.txt
+++ README.txt
@@ -18,11 +18,11 @@ christmasCard2011
 			Bryan Duggan's Programming class, taught to 
 			DT211 first years
 	Tools
-		This project is written in C and uses the Allegro library
-			for graphics
+		This project is written in C and uses the Allegro 
+			library for graphics
 		Allegro 4.4.2
-		The project was originally written with Visual Studio 2011,
-			it's currently being developed with GCC on
+		The project was originally written with Visual Studio 
+			2011, it's currently being developed with GCC on
 			Linux Mint 17
 	
 	Conor Stefanini, 25 May 2015
@@ -56,6 +56,24 @@ Change Log
 
 
 
+	2015-06-08
+		General Details
+			added binary type mode / options
+				still need to comment it better
+		Usage
+			added plus and minus keys to usage
+		Changes to Source
+			fixed formatting to be no more than 72
+				characters per line
+			added binary type options / mode
+		To Do
+			comment and modify mode variable
+		Additional
+			the method seems to work and be a good idea
+				but needs more work and more comments,
+				I'm done for now anyway
+		
+		Conor Stefanini, 8/9 June 2015
 
 
 	2015-05-28
@@ -67,11 +85,13 @@ Change Log
 			'Z' freezes the screen  momentarily and fills it
 				with a random colour
 			'A' creates a "blue sky" effect
-			'S' creates a large XMAS string in an attempted circle
+			'S' creates a large XMAS string in an attempted 
+				circle
 		Changes to Source
-			Added drawXmas function to christmasCard2011.c and
-				commented and formatted it a bit,
-				changed some stuff like '(1 * size)' to 'size'
+			Added drawXmas function to christmasCard2011.c 
+				and commented and formatted it a bit,
+				changed some stuff like '(1 * size)' to 
+				'size'
 			Added drawTrees function to christmasCard2011.c, 
 				commented and formatted it,
 				renamed it drawTree,
@@ -84,7 +104,8 @@ Change Log
 			Added drawCars function
 			Added Blue sky on 'A' key
 			Added big XMAS string on 'S' key
-			Updated the makefile to include the maths library
+			Updated the makefile to include the maths
+				library
 		To Do
 			write a help function (--help)
 			write some command line functionality
@@ -108,14 +129,15 @@ Change Log
 				Modified the two functions slightly
 				Set up the Z key to freeze the screen
 				Set up the programs main screen to run
-			Wrote the "makefile", works when Allegro 4.4.2 is
-				installed
+			Wrote the "makefile", works when Allegro 4.4.2
+				is installed
 			Wrote "tester.c" to test logic of functions in
 				christmasCard2011.c
-			Moved all old files and tester.c to the directory
-				"oldVersions"
+			Moved all old files and tester.c to the
+				directory "oldVersions"
 		To Do
-			Fix the comments in drawXmasBall (remove old version)
+			Fix the comments in drawXmasBall (remove old 
+				version)
 			Fix comments in both functions
 			Continue with implementing old functions
 			Do little movie type things
@@ -134,14 +156,16 @@ Change Log
 				viewed with tab spacing of 8, as github
 				displays it
 		Additional
-			Usage, and To Do sections are the same as previous
-				entry
-			I'm going to use a this version of titles from now on:
-				"2015-05-25_2" == "Year-Month-Date_Version", 
-				this is the second commit / upload today, so
-				the date is underscored as the second version
-			I'm going to consider including the time with date
-				signatures / changelog entries
+			Usage, and To Do sections are the same as 
+				previous entry
+			I'm going to use a this version of titles from 
+				now on: "2015-05-25_2" == 
+				"Year-Month-Date_Version", this is the 
+				second commit / upload today, so the 
+				date is underscored as the second 
+				version
+			I'm going to consider including the time with 
+				date signatures / changelog entries
 		
 		Conor Stefanini, 25 May 2015
 
@@ -151,21 +175,22 @@ Change Log
 			First version of this git project
 			First step in to git projects in general
 			I'm uploading these versions first so that I can
-				build on them through git (seems like the
-				purpose / a strong point of git)
-			These are each version of my Winter 2011 programming
-				assignment done for Bryan Duggan's class
-				in DIT, course DT211
+				build on them through git (seems like 
+				the purpose / a strong point of git)
+			These are each version of my Winter 2011 
+				programming assignment done for 
+				Bryan Duggan's class in DIT, 
+				course DT211
 		Usage
 			The Escape key ends each version of the program
-			The keys q, w, e, r, t, etc. and a, s, d, f, g, etc.
-				do some stuff
+			The keys q, w, e, r, t, etc. and a, s, d, f, g, 
+				etc. do some stuff
 		Changes to Source
 			Fixed minor issues in source code (void main(),
 				return;, etc.)
 			Created this readme file
-			Created an empty makefile (I'm not going to write it
-				for this version)
+			Created an empty makefile (I'm not going to 
+				write it for this version)
 		To Do
 			Read a changelog style guide
 				Set this file up as markdown (maybe)
@@ -174,11 +199,12 @@ Change Log
 				Combine the three files in to one
 				Write a makefile
 				Fix dependencies
-					Move the allegro libraries to the
-						project folder
-				Set up a folder structure (lib, src, bin, or
-					dependencies, source, executables,
-					or something along those lines)
+					Move the allegro libraries to 
+						the project folder
+				Set up a folder structure (lib, src, 
+					bin, or dependencies, source, 
+					executables, or something along 
+					those lines)
 				Fix the style of the final C file
 				Fix all unused variables, etc.
 				Add comments
@@ -190,12 +216,13 @@ Change Log
 
 Additional
 	This file is definitely going to change in structure and purpose
-	This is my first attempt at a version control system and I can already
-		tell that version control does some of the things I'm trying
-		to do through this plain text file
+	This is my first attempt at a version control system and I can 
+		already tell that version control does some of the 
+		things I'm trying to do through this plain text file
 	Either way, the projects that I put on github are going to be
 		documented in some way, even if it's inappropriate or
-		inefficient, I'll learn a better way in time and practise
+		inefficient, I'll learn a better way in time and 
+		practise
 	
 	Conor Stefanini, 25 May 2015
 
--- christmasCard2011.c
+++ christmasCard2011.c
@@ -39,16 +39,16 @@ int drawXmasBall( int centreX, int centreY, int radius, int step,
 	**	Draws a Christmas ball
 	**
 	**	Draws a circle with a centre point (centreX, centreY), 
-	**		a radius described by "int radius", and colouring 
-	**		described by (red, green, blue)
-	**	Then draws circles of decreasing radius within that circle,
-	**		the radius decreases by 
+	**		a radius described by "int radius", and 
+	**		colouring described by (red, green, blue)
+	**	Then draws circles of decreasing radius within that 
+	**		circle, the radius decreases by 
 	**
 	**	int centreX	==	x point for centre of circle
 	**	int centreY	==	y point for centre of circle
 	**	int radius	==	radius of circle
-	**	int step	==	how much the circle decreases by at
-	**					each step
+	**	int step	==	how much the circle decreases by
+	**					at each step
 	**	int red		==	red value for colour of circle
 	**	int green	==	green value for colour of circle
 	**	int blue	==	blue value for colour of circle
@@ -60,7 +60,8 @@ int drawXmasBall( int centreX, int centreY, int radius, int step,
 	//		i.e. circle at (centreX, centreY)
 	//		with radius "radius"
 	//		and coloured (red, green, blue)
-	circle(backBuffer, centreX, centreY, radius, makecol(red, green, blue));
+	circle(backBuffer, centreX, centreY, radius, 
+		makecol(red, green, blue));
 	//inner concentric circles
 	//  radius - spacec = new circles radius
 	int spacec = rand() % 6 + 2;
@@ -77,17 +78,19 @@ int drawXmasBall( int centreX, int centreY, int radius, int step,
 			radius -= spacec;
 		}
 		//new smaller circle
-		circle(backBuffer, centreX, centreY, radius, makecol( red, green, blue));
+		circle(backBuffer, centreX, centreY, radius, makecol( 
+			red, green, blue));
 	}
 	*/
 
 	//	mode
-	//		originally the radius decreased to about a thired of
-	//			the original size and no further
+	//		originally the radius decreased to about a 
+	//			third of the original size and no 
+	//			further
 	//		another option is to decrease to a fixed size
 	//		a minus value for mode represents the original
-	//			version, any other value is the radius the
-	//			circle decreases to
+	//			version, any other value is the radius
+	//			the circle decreases to
 	int mode = -1;
 	
 	//	counter
@@ -98,8 +101,8 @@ int drawXmasBall( int centreX, int centreY, int radius, int step,
 	}
 	
 	//	minimum step
-	//		the minimum value that the circles can decrease by
-	//		step is added to this value
+	//		the minimum value that the circles can decrease
+	//		by step is added to this value
 	int minStep = 2;
 	//int minStep = 80;
 	//int minStep = (rand() % 10) + 2;
@@ -127,18 +130,21 @@ void drawSnowFlake( int x, int y, int radius,
 	/*
 	**	Draw Snow Flake
 	**
-	**	draws a snow flake at (x, y) of size "radius" and coloured
-	**		with (red, green, blue)
-	**	there's a 75 per cent chance of getting a more diamond shaped
-	**		snow flake and a 25 per cent chance of a more cross
-	**		shaped one
+	**	draws a snow flake at (x, y) of size "radius" and 
+	**		coloured with (red, green, blue)
+	**	there's a 75 per cent chance of getting a more diamond 
+	**		shaped snow flake and a 25 per cent chance of a
+	**		more cross shaped one
 	**
 	**	int x		==	x point of centre of snow flake
 	**	int y		==	y point of centre of snow flake
 	**	int radius	==	radius of snow flake
-	**	int red		==	red value for colour of snow flake
-	**	int green	==	green value for colour of snow flake
-	**	int blue	==	blue value for colour of snow flake
+	**	int red		==	red value for colour of snow 
+	**					flake
+	**	int green	==	green value for colour of snow 
+	**					flake
+	**	int blue	==	blue value for colour of snow 
+	**					flake
 	*/
 	
 	
@@ -184,13 +190,18 @@ int drawXmas( int x, int y, int size, int red, int green, int blue )
 	**
 	**	The 'M' of Merry starts at (x + size, y + size)
 	**
-	**	int x		==	x point of top left of "MERRY XMAS"
-	**	int y		==	y point of top left of "MERRY XMAS"
-	**	int size	==	size of "MERRY XMAS" (not exact match
-	**					with pixels)
-	**	int red		==	red value for colour of "MERRY XMAS"
-	**	int green	==	red value for colour of "MERRY XMAS"
-	**	int blue	==	red value for colour of "MERRY XMAS"
+	**	int x		==	x point of top left of "MERRY 
+	**					XMAS"
+	**	int y		==	y point of top left of "MERRY 
+	**					XMAS"
+	**	int size	==	size of "MERRY XMAS" (not exact 
+	**					match with pixels)
+	**	int red		==	red value for colour of "MERRY 
+	**					XMAS"
+	**	int green	==	red value for colour of "MERRY 
+	**					XMAS"
+	**	int blue	==	red value for colour of "MERRY 
+	**					XMAS"
 	*/
 	
 	//	'M'
@@ -320,22 +331,26 @@ int drawXmas( int x, int y, int size, int red, int green, int blue )
 
 int trimCol( int colourValue )
 {
-	//	trim colour
-	//
-	//	this function is used to keep numbers between 0, and 255
-	//	values from 0 to 255 are used as red, green, or blue values
-	//		to make colours
-	//	this function is mostly used by the drawTree function below
-	//
-	//	the function takes in the possible colour value, and 
-	//		returns the new appropriate colour value
-	//
-	//	the function checks if the passed number is below zero or 
-	//		above 255, if it is below zero, for example '-50'
-	//		then the new colour is 205 (-50 + 255, or 255 - 50),
-	//		if the number is above 255, for example 300, then the
-	//		new number is 45, (300 - 255)	
-	//	it also works for ridiculously big numbers (just in case)
+	/*
+	**	trim colour
+	**
+	**	this function is used to keep numbers between 0, and 255
+	**	values from 0 to 255 are used as red, green, or blue 
+	**		values to make colours
+	**	this function is mostly used by the drawTree function 
+	**		below
+	**	the function takes in the possible colour value, and 
+	**		returns the new appropriate colour value
+	**
+	**	the function checks if the passed number is below zero 
+	**		or above 255, if it is below zero, for example 
+	**		'-50' then the new colour is 205 (-50 + 255, or 
+	**		255 - 50), if the number is above 255, for 
+	**		example 300, then the new number is 45, 
+	**		(300 - 255)	
+	**	it also works for ridiculously big numbers (just in 
+	**		case)
+	*/
 	
 	if (colourValue > 255) {
 		colourValue = colourValue % 255;
@@ -355,16 +370,16 @@ int drawTree( int x, int y, int radius, int red, int green, int blue )
 	**
 	**	Draws a Christmas Tree at (x, y) of size 'size' and 
 	**		colouring (red, green, blue)
-	**	The tree is drawn with two triangles (for the  leaves / top)
-	**		and a rectangle for the trunk
+	**	The tree is drawn with two triangles (for the 
+	**		leaves / top) and a rectangle for the trunk
 	**	The colouring is a bit weird, basically, if a dark green
-	**		(34, 139, 34) is passed through (red, green, blue),
-	**		then the resulting trunk colour is brown
+	**		(34, 139, 34) is passed through (red, green, 
+	**		blue), then the resulting trunk colour is brown
 	*/
 	
-	//	the trunk colours are odd, if the passed (red, green, blue)
-	//		are (34, 139, 34) (dark green), then the trunk is
-	//		brown
+	//	the trunk colours are odd, if the passed (red, green, 
+	//		blue) are (34, 139, 34) (dark green), then the 
+	//		trunk is brown
 	//	trimCol makes sure the numbers are between 0, and 255
 	int trunkRed = trimCol(red + 105);
 	int trunkGreen = trimCol(green - 70);
@@ -418,8 +433,10 @@ int drawCar( int x, int y, int red, int green, int blue )
 	line(backBuffer, (x + 30), (y), (x + 35), (y + 7), 
 		makecol(red, green, blue));
 	//	wheels
-	circle(backBuffer, (x + 10), (y + 15), 5, makecol(red, green, blue));
-	circle(backBuffer, (x + 35), (y + 15), 5, makecol(red, green, blue));
+	circle(backBuffer, (x + 10), (y + 15), 5, 
+		makecol(red, green, blue));
+	circle(backBuffer, (x + 35), (y + 15), 5, 
+		makecol(red, green, blue));
 	
 	return 0;
 }
@@ -456,6 +473,11 @@ int main( int argc, char *argv[] )
 	
 	float angle = 0;
 	
+	int mode = 3;	//	mode for running (will be explained
+			//		later)
+	
+	int keyFlag = 0;//	for keyboard pressing (one instance)
+	
 	
 	
 	/*
@@ -465,7 +487,7 @@ int main( int argc, char *argv[] )
 	//	initialise allegro, ends program if unsuccessful
 	if (allegro_init()) {
 		fprintf(stderr, "Couldn't initialise Allegro\n"
-			"Check that Allegro version 4.4.2 is installed\n");
+			"Check that Allegro 4.4.2 is installed\n");
 		return EXIT_FAILURE;
 	}
 	
@@ -477,20 +499,21 @@ int main( int argc, char *argv[] )
 	//	set graphics / screen style (windowed, or fullscreen)
 	if (set_gfx_mode(GFX_AUTODETECT_WINDOWED, w, h, 0, 0)) {
 		fprintf(stderr, "Couldn't set graphics mode\n"
-			"Check that Allegro version 4.4.2 is installed\n");
+			"Check that Allegro 4.4.2 is installed\n");
 		return EXIT_FAILURE;
 	}
 	
-	//	create the blueck buffer
-	//		the blueck buffer is a bitmap image
-	//		all graphics get written to the blueck buffer, and
-	//			then on to the screen, this avoids flickering
+	//	create the back buffer
+	//		the back buffer is a bitmap image
+	//		all graphics get written to the back buffer, and
+	//			then on to the screen, this avoids 
+	//			flickering
 	backBuffer = create_bitmap(w, h);
 
 	//	set up the keyboared
 	if (install_keyboard()) {
 		fprintf(stderr, "Couldn't install the keyboared\n"
-			"Check that Allegro version 4.4.2 is installed\n");
+			"Check that Allegro 4.4.2 is installed\n");
 		return EXIT_FAILURE;
 	}
 	
@@ -506,84 +529,209 @@ int main( int argc, char *argv[] )
 			break;
 		}
 		
-		//	set the blueckground colour to black each frame,
-		//		the screen gets written at the end of the
-		//		loop, each frame
+		//	set the background colour to black each frame,
+		//		the screen gets written at the end of
+		//		the loop, each frame
 		clear_to_color(backBuffer, makecol(0, 0, 0));
 		
 		
 		//	idea for feature implementation
-		//		treat it like a binary interpretation of a
-		//		decimal, like chmod settings, i.e.:
+		//		treat it like a binary interpretation of
+		//		a decimal, like chmod settings, i.e.:
 		//		0000 0 	==	nothing
 		//		0001 1	==	xmas balls
 		//		0011 3	==	xmas balls and snow
-		//		0111 7	==	xmas balls, snow, XMAS string
-		//		0101 5	==	xmas balls and XMAS string
+		//		0111 7	==	xmas balls, snow, XMAS
+		//					string
+		//		0101 5	==	xmas balls and XMAS 
+		//					string
 		
 		
+		//	cars	land	trees	XMAS	snow	balls
+		//	c	l	t	x	s	b
+		//	0	0	0	0	0	0
+		//	000000
 		
-		//	Christmas balls
-		x = rand() % w;
-		y = rand() % h;
-		col = rand() % 256;	//	colour values go from 0 to 255
-		rad = rand() % 700;
-		drawXmasBall(x, y, rad, 6, col, 0, 0);
-		x = rand() % w;
-		y = rand() % h;
-		col = rand() % 256;
-		rad = rand() % 700;
-		drawXmasBall(x, y, rad, 6, 0, col, 0);
-		//	the original did 3
-		x = rand() % w;
-		y = rand() % h;
-		col = rand() % 256;
-		rad = rand() % 700;
-		drawXmasBall(x, y, rad, 6, col, 0, 0);
+		//	000000	0	==	nothing
+		//	000001	1	==	b
+		//	000010	2	==	s
+		//	000011	3	==	sb
+		//	000100	4	==	x
+		//	000101	5	==	xb
+		//	000110	6	==	xs
+		//	000111	7	==	xsb
+		//	001000	8	==	
+		//	001001	9	==	
+		//	001010	10	==	
+		//	001011	11	==	
+		//	001100	12	==	
+		//	001101	13	==	
+		//	001110	14	==	
+		//	001111	15	==	
+		//	010000	16	==	
+		//	010001	17	==	
+		//	010010	18	==	
+		//	010011	19	==	
+		//	010100	20	==	
+		//	010101	21	==	
+		//	010110	22	==	
+		//	010111	23	==	
+		//	011000	24	==	
+		//	011001	25	==	
+		//	011010	26	==	
+		//	011011	27	==	
 		
-		//	Snow
-		x = rand() % w;
-		y = rand() % h;
-		rad = (rand() % 15) + 2;	//	original rad value
-		drawSnowFlake(x, y, rad, 255, 255, 255);
-		x = rand() % w;
-		y = rand() % h;
-		rad = (rand() % 10) + 2;	//	original rad value
-		drawSnowFlake(x, y, rad, 255, 255, 255);
-		x = rand() % w;
-		y = rand() % h;
-		rad = (rand() % 10) + 2;	//	original rad value
-		drawSnowFlake(x, y, rad, 255, 255, 255);
-		x = rand() % w;
-		y = rand() % h;
-		rad = (rand() % 5) + 2;		//	original rad value
-		drawSnowFlake(x, y, rad, 255, 255, 255);
+		/*
+		**	FEATURES LIST
+		**		Xmas Balls 	3 blue, and green balls
+		**		
+		**		Snow big	4 snowflakes of varying 
+		**					size
+		**		Snow small	smaller snowflakes for
+		**					landscape
+		**		XMAS big	big version of MERRY 
+		**					XMAS string
+		**		XMAS small	mini version for 
+		**					landscape shot
+		**		Trees small	small trees on the land-
+		**					scape
+		**		Blue sky	2 big blue balls
+		**
+		**		Land		white rectangle on 
+		**					bottom
+		**		Cars		to be configured
+		*/
 		
-		//	X-Mas
-		x = w / 2;
-		y = h / 2;
-		rad = 100;
-		r = rand() % 256;
-		g = rand() % 256;
-		b = rand() % 256;
-		putpixel(backBuffer, x, y, makecol(r, g, b));
-//		line(backBuffer, x, y, x + rad, y, makecol(r, g, b));
-//		line(backBuffer, x, y, x, y + rad, makecol(r, g, b));
-//		
-//		line(backBuffer, x + rad, y, x + rad, y + rad, makecol(r, g, b));
-//		line(backBuffer, x, y + rad, x + rad, y + rad, makecol(r, g, b));
+		if (mode & 1) {
+			/*
+			**	Christmas balls
+			*/
+			
+			x = rand() % w;
+			y = rand() % h;
+			//	colour values go from 0 to 255
+			col = rand() % 256;
+			rad = rand() % 700;
+			drawXmasBall(x, y, rad, 6, col, 0, 0);
+			
+			x = rand() % w;
+			y = rand() % h;
+			col = rand() % 256;
+			rad = rand() % 700;
+			drawXmasBall(x, y, rad, 6, 0, col, 0);
+			
+			//	the original did 3
+			x = rand() % w;
+			y = rand() % h;
+			col = rand() % 256;
+			rad = rand() % 700;
+			drawXmasBall(x, y, rad, 6, col, 0, 0);
+		}
 		
-//		drawXmas(x, y, rad, r, g, b);
+		if (mode & 2) {
+			/*
+			**	Snow (Big)
+			*/
+			
+			x = rand() % w;
+			y = rand() % h;
+			//	value for rad taken from original source
+			//		code
+			rad = (rand() % 15) + 2;	
+			drawSnowFlake(x, y, rad, 255, 255, 255);
+			
+			x = rand() % w;
+			y = rand() % h;
+			//	value for rad taken from original source
+			//		code
+			rad = (rand() % 10) + 2;
+			drawSnowFlake(x, y, rad, 255, 255, 255);
+			
+			x = rand() % w;
+			y = rand() % h;
+			//	value for rad taken from original source
+			//		code
+			rad = (rand() % 10) + 2;
+			drawSnowFlake(x, y, rad, 255, 255, 255);
+			
+			x = rand() % w;
+			y = rand() % h;
+			rad = (rand() % 5) + 2;
+			drawSnowFlake(x, y, rad, 255, 255, 255);
+		}
 		
-		//drawTree(x, y, rad, 34, 139, 34);
-		//drawTree(x, y, rad, 0, 150, 0);
+		if (mode & 4) {
+			/*
+			**	XMAS String (big)
+			*/
+			
+			x = w / 2;
+			y = h / 2;
+			rad = 100;
+			r = rand() % 256;
+			g = rand() % 256;
+			b = rand() % 256;
+			drawXmas(x, y, rad, r, g, b);
+		}
+		
+		if (mode & 8) {
+			/*
+			**	Blue Sky
+			*/
+		}
+		
+		if (mode & 16) {
+			/*
+			**	Land
+			*/
 
-		//drawCar(x, y, r, g, b);
+			rectfill(backBuffer, 0, 300, w, h, 
+				makecol(255, 255, 255));
+		}
+		
+		if (mode & 32) {
+			/*
+			**	Snow (Small)
+			*/
+			
+			int randx = rand() % 641;
+			int randy = rand() % 481;
+			int randrad = rand() % 3 + 1;
+			drawSnowFlake(randx, randy, randrad, 255, 255, 255);
+			int i = 0;
 
+			while(i<=randx)
+			{
+				int randx1 = rand() % 641;
+				int randy1 = rand() % 481;
+				int randrad1 = rand() % 3 + 1;
+				drawSnowFlake(randx1, randy1, randrad1, 255, 255, 255);
+				i++;
+			}
+		}
+		
+		
+		if (mode & 64) {
+			/*
+			**	XMAS String (Small)
+			*/
+		}
+		
+		if (mode & 128) {
+			/*
+			**	Trees (Small)
+			*/
+		}
+		
+		if (mode & 256) {
+			/*
+			**	Cars
+			*/
+		}
 
 		
 		/*
-		**	input keys
+		**	Input Keys
 		*/
 		
 		//	freeze screen
@@ -606,16 +754,20 @@ int main( int argc, char *argv[] )
 			for (i = 0; i < 2; ++i) {
 				x = rand() % w;
 				y = rand() % h;
-				//	big radius to cover the whole screen
+				//	big radius to cover the whole 
+				//		screen
 				rad = rand() % 1000;
-				//	fill circles right down to centre
-				//		2000 from original program
+				//	fill circles right down to 
+				//		centre, 2000 from 
+				//		original program
 				for (j = 0; j < 2000; ++j) {
-					//	random blue values, gives a 
-					//		darker overall colour
+					//	random blue values, 
+					//		gives a darker 
+					//		overall colour
 					rad = rand() % 1000;
 					b = rand() % 256;
-					//	circles of decreasing radius
+					//	circles of decreasing 
+					//		radius
 					circle(backBuffer, x, y, rad, 
 						makecol(0, 0, b));
 				}
@@ -649,12 +801,33 @@ int main( int argc, char *argv[] )
 				angle = 0;
 			}
 		}
-			
 		
 		
+		if (key[KEY_MINUS]) {
+			if ((mode > 0) && (!keyFlag)) {
+				keyFlag = 1;
+				mode--;
+			}
+		}
+		if (key[KEY_EQUALS]) {
+			if (!keyFlag) {
+				keyFlag = 1;
+				mode++;
+			}
+		}
+		
+		if ((!key[KEY_MINUS]) && (!key[KEY_EQUALS])) {
+			keyFlag = 0;
+		}
 		
 		
-		//	writes the blueck buffer to the screen
+		//	debug
+		textprintf_centre_ex(backBuffer, font, 
+			w / 2, h / 2, makecol(255, 255, 255), 
+			-1, "mode %d", mode);
+		
+		
+		//	writes the back buffer to the screen
 		blit(backBuffer, screen, 0, 0, 0, 0, w, h);
 	}
 	
